7d108c1f1ae7ebd0151807b109ff23d6a9aac84d
Jessica Nordlund <jessicanordlund@yahoo.com>
2020-05-11 19:39:01 +0200

    Update Panel_C-D.R

--- Figures/FigureX-JN/Panel_C-D.R
+++ Figures/FigureX-JN/Panel_C-D.R
@@ -1,6 +1,7 @@
 #Panels C-D of the Assay comparison figure:
 
 library(ggplot2)
+library(dplyr)
 library(reshape2)
 library(RColorBrewer)
 
@@ -23,26 +24,30 @@ my.genome.cols <- c("HG001" = "#3aa142",
 # FOR HG002, load the beta-values of all CpG sites >=10x coverage from down-sampled Bedgraph files to 20x. 
 dat <- read.table(gzfile("/Users/jessicanordlund/Downloads/EPIQC/beta_HG002_LAB01_10x.txt.gz"), 
                  header= TRUE, fill=TRUE)  
-
-#Randomly sample 1M CGs:
-dat <- dat[sample(nrow(dat), 1000000), ]
+count.cpgs.covered <- colSums(is.na(dat))
 
 indx <- grepl('EPIC', colnames(dat)) #Remove EPIC
 dat <- dat[, !indx]
 
 names(dat) <- c("TruSeq", "SPLAT", "EMSeq", "MethylSeq", "TrueMethyl") #Make clean names
+count.cpgs.covered <- colSums(!is.na(dat))
+
+
+#Randomly sample 1M CGs: otherwise it takes too long
+dat <- dat[sample(nrow(dat), 1000000), ]
+
+# format data for plotting
 dat$CG <- row.names(dat) #add CG row name as ID 
 tmp <- as_tibble(melt(dat, id="CG")) # Melt it 
 tmp.cols <- my.assay.cols[match(names(dat), names(my.assay.cols))] # fix color order for plots
-
+  
 # Get data, calculate correlations
 df <- dat[,1:3]
 my.cor.p <- round(cor(df, use = "pairwise.complete.obs", method="pearson"), 2)
 my.cor.s <- round(cor(df, use = "pairwise.complete.obs", method="spearman"), 2)
 
-
-
-# PLOT THE SMOOTH SCATTER PLOTS
+# ----------------------------------------------------#
+# PLOT THE SMOOTH SCATTER PLOTS- PANEL C
 # Build colors
 buylrd = c("#313695", "#4575B4", "#74ADD1", "#ABD9E9", "#E0F3F8", "#FFFFBF",
            "#FEE090", "#FDAE61", "#F46D43", "#D73027", "#A50026") 
@@ -69,11 +74,22 @@ for(i in 1:ncol(combs)){
   legend("topleft", legend=legend.text, cex=0.9, bty="n", text.col="white")
   }
 
+#-------------------------------------------------------------------------------#
 
-# PLOT the Beta-value distribution
-ggplot(tmp, aes(x=value, color=variable))+
+
+
+#
+# PLOT the Beta-value distribution: Panel D
+tmp <- rename(tmp, Assay = variable)
+
+ggplot(tmp, aes(x=value, color=Assay))+
   geom_density() +
-  scale_color_manual(values= tmp.cols)
+  scale_color_manual(values= tmp.cols, 
+                    labels = paste0(levels(tmp$Assay), " (", round(count.cpgs.covered/1000000, 1), ")"))+
+  xlab("beta-value") +
+  theme(legend.position="top", panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+        panel.background = element_blank(), axis.line = element_line(colour = "black"))
 
 
+  
 
