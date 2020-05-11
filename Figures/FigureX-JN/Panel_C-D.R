713c1eebf4dba52b839fa03b2fdc61119dd8b6fb
Jessica Nordlund <jessicanordlund@yahoo.com>
2020-05-11 09:47:05 +0200

    Update Panel_C-D.R

--- Figures/FigureX-JN/Panel_C-D.R
+++ Figures/FigureX-JN/Panel_C-D.R
@@ -1,1 +1,79 @@
+#Panels C-D of the Assay comparison figure:
+
+library(ggplot2)
+library(reshape2)
+library(RColorBrewer)
+
+my.assay.cols <- c("MethylSeq" = "#377eb8", 
+                   "TruSeq" = "#e41a1c",
+                   "EMSeq" = "#4daf4a",
+                   "SPLAT" = "#984ea3",
+                   "EPIC" = "#ff7f00",
+                   "Arrays" = "#ffff33",
+                   "TrueMethyl" = "#a65628")
+
+my.genome.cols <- c("HG001" = "#3aa142",
+                    "HG002" = "#bdd7e7",
+                    "HG003" = "#6baed6",
+                    "HG004" = "#2171b5",
+                    "HG005" = "#fcae91",
+                    "HG006" = "#fb6a4a",
+                    "HG007" = "#cb181d")
+
+# FOR HG002, load the beta-values of all CpG sites >=10x coverage from down-sampled Bedgraph files to 20x. 
+dat <- read.table(gzfile("/Users/jessicanordlund/Downloads/EPIQC/beta_HG002_LAB01_10x.txt.gz"), 
+                 header= TRUE, fill=TRUE)  
+
+#Randomly sample 1M CGs:
+dat <- dat[sample(nrow(dat), 1000000), ]
+
+indx <- grepl('EPIC', colnames(dat)) #Remove EPIC
+dat <- dat[, !indx]
+
+names(dat) <- c("TruSeq", "SPLAT", "EMSeq", "MethylSeq", "TrueMethyl") #Make clean names
+dat$CG <- row.names(dat) #add CG row name as ID 
+tmp <- as_tibble(melt(dat, id="CG")) # Melt it 
+tmp.cols <- my.assay.cols[match(names(dat), names(my.assay.cols))] # fix color order for plots
+
+# Get data, calculate correlations
+df <- dat[,1:3]
+my.cor.p <- round(cor(df, use = "pairwise.complete.obs", method="pearson"), 2)
+my.cor.s <- round(cor(df, use = "pairwise.complete.obs", method="spearman"), 2)
+
+
+
+# PLOT THE SMOOTH SCATTER PLOTS
+# Build colors
+buylrd = c("#313695", "#4575B4", "#74ADD1", "#ABD9E9", "#E0F3F8", "#FFFFBF",
+           "#FEE090", "#FDAE61", "#F46D43", "#D73027", "#A50026") 
+myColRamp = colorRampPalette(c(buylrd))
+
+# Get all info organized
+combs <- combn(1:ncol(df), 2) 
+my.cor.p<- melt(my.cor.p)
+my.cor.s <- melt(my.cor.s)
+
+#Make the plot
+par(mfrow=c(2,2), mar=c(3,4,1,1), mgp=c(1.5, .5, 0))
+
+for(i in 1:ncol(combs)){
+  x.name <-names(df)[combs[1,i]]
+  y.name <- names(df)[combs[2,i]]
+  cor.text.nr <- which(my.cor.p$Var1 %in% x.name & my.cor.p$Var2 %in% y.name)
+  legend.text <- c(sprintf("r=%s",my.cor.p$value[cor.text.nr]), sprintf("p=%s",my.cor.s$value[cor.text.nr]))
+  
+  smoothScatter(x=df[,combs[1,i]], y=df[,combs[2,i]],
+                xlab=x.name,ylab=y.name,
+                colramp=myColRamp)
+  
+  legend("topleft", legend=legend.text, cex=0.9, bty="n", text.col="white")
+  }
+
+
+# PLOT the Beta-value distribution
+ggplot(tmp, aes(x=value, color=variable))+
+  geom_density() +
+  scale_color_manual(values= tmp.cols)
+
+
 
