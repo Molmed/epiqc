cb7305aa94e256835de24662beb4682e99a980c6
jessieno <jessicanordlund@yahoo.com>
2020-06-17 15:12:29 +0200

    now script runs on HPC

--- Figures/SupplementaryFig_X_JN.R
+++ Figures/SupplementaryFig_X_JN.R
@@ -3,17 +3,22 @@
 # Based on Downsampled BedGraphs to 10x average coverage
 # CpG site inclusion requirement is >= 5x coverage
 
-
+suppressPackageStartupMessages()
 library(data.table)
-library(ggplot2)
-library(dplyr)
+library(ggplot2) 
+library(dplyr) 
 library(reshape2)
 library(RColorBrewer)
-library(hexbin)
+#library(hexbin)
 library(tidyr)
 library(GGally)
+library(ggbeeswarm)
+library(corrplot)
+options(stringsAsFactors = FALSE)
 
-setwd("/Users/jessicanordlund/Downloads/EPIQC/for_manus")
+
+# Set working directory
+setwd("/proj/uppstore2017167/EPIQC/plots_for_manuscript/plot")
 
 
 # FUNCTIONS #
@@ -52,85 +57,147 @@ my.genome.cols <- c("HG001" = "#3aa142",
 
 # LOAD DATA & ORGANIZE IT #
 # OBS TO DO, make a loop to parse through all genomes. 
-
 # load the beta-values of all CpG sites >=10x coverage from down-sampled Bedgraph files to 20x. 
-f <- '/Users/jessicanordlund/Downloads/EPIQC/10xDS/beta_HG001_LAB01_5x.txt.gz'
-my.dat <- fread(f)
 
-tmp.name <- sub("_LAB01_[[:digit:]]+x.txt.gz", "", basename(f))
-tmp.name <- sub("beta_", "", tmp.name)
+#----------------------------#
+# GET THE DATA and format it #
+#----------------------------#
+dat.dir <- '/proj/uppstore2017167/EPIQC/plots_for_manuscript/10xDS_bedgraphs'
+my.files <- list.files(dat.dir, recursive=F, full.names= T, pattern= "[[:digit:]]+x.txt.gz")
 
-my.dat <- as.data.frame(my.dat)
-indx <- grepl('EPIC', names(my.dat)) #Remove EPIC
-my.dat<- my.dat[, !indx]
-
-names(my.dat) <- gsub("_HG.*", "", names(my.dat))
-names(my.dat) <- my.names$NEW[match(names(my.dat), my.names$OLD)] # clean names
-my.dat <- my.dat[,order(names(my.dat))] #alphabetical order
-
-
-# Calculate genome-wide correlations, RMSE and Pair-wise overlapping sites
-count.cpgs.covered <- colSums(!is.na(my.dat)) #COunt CPG sites 
-my.cor.p <- round(cor(my.dat, use = "pairwise.complete.obs", method="pearson"), 3)
-my.cor.s <- round(cor(my.dat, use = "pairwise.complete.obs", method="spearman"), 3)
-
-cor.p <- reshape2::melt(my.cor.p)
-cor.s <- reshape2::melt(my.cor.s)
-
-# Calc RMSE
-combs <- combn(1:ncol(my.dat), 2) 
-datalist = list()
-
-for (i in 1:ncol(combs)) {
-  x <- combs[1,i]
-  y <- combs[2,i]
-  datalist[[i]] <- c(names(my.dat)[x], names(my.dat)[y], 
-                     round(RMSE(my.dat[,x],my.dat[,y]), 3),
-                     sum(!is.na(my.dat[,x] & my.dat[,y])))
-}
-
-rmse_data = as.data.frame(do.call(rbind, datalist))
-names(rmse_data) <- c("Lib1", "Lib2", "rmse", "nsites")
+my.stats <- as.list(NULL)
+my.cpgs <- as.list(NULL)
+my.cor.p <- as.list(NULL)
+my.cor.s <- as.list(NULL)
+my.rmse <- as.list(NULL)
+num.sites <- "100000"
+large.df <- data.frame()[1:num.sites, ]
 
 
-#Randomly sample 10k CGs to plot the smooth scatter: otherwise it takes too long to plot
-df <- as.data.frame(my.dat[sample(nrow(my.dat), 10000), ])
 
+#----------------------------#
+# Loop though by genome #
+#----------------------------#
 
-# PLOT
-p <- ggpairs(df, lower="blank", upper = "blank") 
-seq <- 1:ncol(df)
-for (x in seq)
-  for (y in seq) 
-    if (y>x) 
-      p <- putPlot(p, ggplot(df, aes_string(x=names(df)[x],y=names(df)[y])) + 
-                     stat_density2d(aes(fill = ..density..^0.55), geom = "tile", contour = FALSE, n = 200) +
-                     theme(axis.text.x = element_text(angle = 90))+
-                     scale_fill_viridis_c(option = "plasma"), y,x)
+for (f in my.files) {
+  my.dat <- fread(f)
+  
+  # 1 fix names & order
+  {
+  tmp.name <- sub("_LAB01_[[:digit:]]+x.txt.gz", "", basename(f))
+  tmp.name <- sub("beta_", "", tmp.name)
+  
+  print(c(tmp.name, "is loaded"))
+  
+  my.dat <- as.data.frame(my.dat)
+  indx <- grepl('EPIC', names(my.dat)) #Remove EPIC
+  my.dat<- my.dat[, !indx]
+  
+  names(my.dat) <- gsub("_HG.*", "", names(my.dat))
+  names(my.dat) <- my.names$NEW[match(names(my.dat), my.names$OLD)] # clean names
+  my.dat <- my.dat[,order(names(my.dat))] #alphabetical order
+  }
 
-for (x in seq) 
-  for (y in seq) 
-    if (x>y) {
-      rmse.test <- as.character(rmse_data$rmse[rmse_data$Lib1 %in% names(df)[x] & rmse_data$Lib2 %in% names(df)[y] |
+  # 2 add a subset to the large dataframe - for the corr plot
+  {
+  tmp <- my.dat[1:num.sites ,]
+  names(tmp) <- paste(tmp.name,names(tmp), sep="_")
+  large.df <- cbind(large.df, tmp)
+  rm(tmp)
+  }
+
+  
+  # 3 basic statistics
+  {
+  print("getting basic stats")
+  my.stats[[tmp.name]] <- apply(my.dat, 2, summary, digits = 3) 
+  my.cpgs[[tmp.name]] <- colSums(!is.na(my.dat))
+  }
+  
+  
+  # 4 calculating correlations
+  {
+  print("Calculating Correlation")
+  cor.p <- round(cor(my.dat, use = "pairwise.complete.obs", method="pearson"), 3)
+  cor.s <- round(cor(my.dat, use = "pairwise.complete.obs", method="spearman"), 3)
+  
+  my.cor.p[[tmp.name]] <- reshape2::melt(cor.p)
+  my.cor.s[[tmp.name]] <- reshape2::melt(cor.s)
+  }
+  
+  
+  # 5 calculating RMSE 
+  {
+    print("Calculating RMSE")
+    combs <- combn(1:ncol(my.dat), 2) 
+    datalist = list()
+  
+    for (i in 1:ncol(combs)) {
+      x <- names(my.dat)[combs[1,i]]
+      y <- names(my.dat)[combs[2,i]]
+      datalist[[i]] <- c(x, y, 
+                       round(RMSE(my.dat[,x],my.dat[, y]), 3),
+                       sum(!is.na(my.dat[,x] & my.dat[,y])))
+    }
+  
+    rmse_data <- as.data.frame(do.call(rbind, datalist))
+    names(rmse_data) <- c("Lib1", "Lib2", "rmse", "nsites")
+    my.rmse[[tmp.name]] <- rmse_data
+  }
+  
+  # 6 plotting
+  #Randomly sample 10k CGs to plot the smooth scatter: otherwise it takes too long to plot
+  {
+    print("plotting")
+    df <- as.data.frame(my.dat[sample(nrow(my.dat), 10000), ])
+
+    p <- ggpairs(df, lower="blank", upper = "blank") 
+    seq <- 1:ncol(df)
+  
+    for (x in seq)
+      for (y in seq) 
+        if (y>x){
+          p <- putPlot(p, ggplot(df, aes_string(x=names(df)[x],y=names(df)[y])) + 
+                         stat_density2d(aes(fill = ..density..^0.55), geom = "tile", contour = FALSE, n = 200) +
+                         theme(axis.text.x = element_text(angle = 90))+
+                         scale_fill_viridis_c(option = "plasma"), y,x)
+        } 
+        
+
+   for (x in seq) 
+      for (y in seq) 
+        if (x>y) {
+          rmse.test <- as.character(rmse_data$rmse[rmse_data$Lib1 %in% names(df)[x] & rmse_data$Lib2 %in% names(df)[y] |
                    rmse_data$Lib2 %in% names(df)[x] & rmse_data$Lib1 %in% names(df)[y]])
-      n.sites <- as.character(rmse_data$nsites[rmse_data$Lib1 %in% names(df)[x] & rmse_data$Lib2 %in% names(df)[y] |
+          n.sites <- as.character(rmse_data$nsites[rmse_data$Lib1 %in% names(df)[x] & rmse_data$Lib2 %in% names(df)[y] |
                   rmse_data$Lib2 %in% names(df)[x] & rmse_data$Lib1 %in% names(df)[y]])
-      tmp.p <- cor.p$value[cor.p$Var1 %in% names(df)[x] & cor.p$Var2 %in% names(df)[y]]
-      tmp.s <- cor.s$value[cor.s$Var1 %in% names(df)[x] & cor.s$Var2 %in% names(df)[y]]
+          tmp.my.cor.p <- my.cor.p[[tmp.name]]
+          tmp.my.cor.s <- my.cor.s[[tmp.name]]
+          tmp.p <- tmp.my.cor.p$value[tmp.my.cor.p$Var1 %in% names(df)[x] & tmp.my.cor.p$Var2 %in% names(df)[y]]
+          tmp.s <- tmp.my.cor.s$value[tmp.my.cor.s$Var1 %in% names(df)[x] & tmp.my.cor.s$Var2 %in% names(df)[y]]
      
         
-      p <- putPlot(p, ggplot(df, aes_string(x=names(df)[x],y=names(df)[y])) + 
+         p <- putPlot(p, ggplot(df, aes_string(x=names(df)[x],y=names(df)[y])) + 
                      theme(panel.background = element_blank(), 
                            axis.title = element_blank(),
                            axis.text = element_blank(), 
                            axis.ticks = element_blank()) +
                      annotate("text", x=1, y=2,size =3, 
-                              label= sprintf("r= %s \np= %s \nrmse= %s \n%s CpGs",tmp.p, tmp.s, rmse.test, n.sites)), y,x)}
+                              label= sprintf("r= %s \np= %s \nrmse= %s \n%s CpGs",
+                                             tmp.p, tmp.s, rmse.test, n.sites)), y,x)
+         }
+
+    
+
+    png(filename= sprintf("%s_AssayComparison.png", tmp.name), 
+        width=1300, height=850, res=200, bg = "transparent")
+     p
+    dev.off()
+  }
+
+}
 
 
-png(filename= sprintf("%s_AssayComparison.png", tmp.name), 
-    width=1300, height=850, res=200, bg = "transparent")
-p
-dev.off()
+save(my.stats,my.cpgs, my.cor.p, my.cor.s, my.rmse, large.df, file = "Assay_compare_data.RData")
 
 
