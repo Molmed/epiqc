0419814fe0cc0ac7b6591bfd325fe4835e8df764
jessieno <jessicanordlund@yahoo.com>
2020-07-04 11:46:52 +0200

    updates to the upset script

--- Figures/FigureX-JN/UpSet_Annot_prep.R
+++ Figures/FigureX-JN/UpSet_Annot_prep.R
@@ -26,13 +26,6 @@ for (f in my.files) {
   my.splat <- cbind(my.splat, df %>% select(contains("SPLAT")))
 }
 
-# EMSeq
-my.emseq[, sums := rowSums(.SD)]
-fwrite(anot[with(my.emseq, sums < 1),], file="emseq_coords_missed.bed", sep="\t", col.names=F)
-
-my.splat[, sums := rowSums(.SD)]
-fwrite(anot[with(my.splat, sums >=3),], file="splat_coords.bed", sep="\t",col.names=F)
-
 
 #------------------------------------------------#
 # UPSET PLOTS
@@ -93,3 +86,116 @@ upset(my.truemethyl,
       sets.x.label = "N CpG sites", 
       text.scale = c(1.2, 1.2, 1.2, 1.2, 1.1, 0.7))
 dev.off()
+
+# Write files for CPGs "missed" at 5X cov in all libraries aka "MISSED"
+# and for CpGs "catched" at 5X or greater in most libraries aka "Catched"
+my.emseq[, sums := rowSums(.SD)]
+my.splat[, sums := rowSums(.SD)]
+my.methylseq[, sums := rowSums(.SD)]
+my.truemethyl[, sums := rowSums(.SD)]
+my.truseq[, sums := rowSums(.SD)]
+
+fwrite(anot[with(my.emseq, sums < 1),], file="emseq_coords_missed.bed", sep="\t", col.names=F)
+fwrite(anot[with(my.emseq, sums >= 4),], file="emseq_coords_catched.bed", sep="\t", col.names=F)
+
+fwrite(anot[with(my.splat, sums < 1),], file="splat_coords_missed.bed", sep="\t",col.names=F)
+fwrite(anot[with(my.splat, sums >= 4),], file="splat_coords_catched.bed", sep="\t",col.names=F)
+
+fwrite(anot[with(my.methylseq, sums < 1),], file="methylseq_coords_missed.bed", sep="\t", col.names=F)
+fwrite(anot[with(my.methylseq, sums >= 4),], file="methylseq_coords_catched.bed", sep="\t", col.names=F)
+
+fwrite(anot[with(my.truemethyl, sums < 1),], file="truemethyl_coords_missed.bed", sep="\t",col.names=F)
+fwrite(anot[with(my.truemethyl, sums >= 4),], file="truemethyl_coords_catched.bed", sep="\t",col.names=F)
+
+fwrite(anot[with(my.truseq, sums < 1),], file="truseq_coords_missed.bed", sep="\t",col.names=F)
+fwrite(anot[with(my.truseq, sums >= 4),], file="truseq_coords_catched.bed", sep="\t",col.names=F)
+
+# EM-Seq 
+#missed:3254409
+#catched:15908511
+
+# MethylSeq
+#missed:3969935
+#catched:15741190
+
+# Splat
+#missed:5542927
+#catched:14139319
+
+#Truemethyl
+#missed:3124316
+#catched: 15801320
+
+#TruSeq
+#missed:10157283
+#catched:12063788
+
+
+# NEW Upsets for: Never caught, usually caught and always caught
+merged.dat <- data.table(emseq = select(my.emseq, sums), 
+           methylseq= select(my.methylseq, sums),
+           splat= select(my.splat, sums),
+           truemethyl=select(my.truemethyl, sums), 
+           truseq=select(my.truseq, sums))
+
+# Never caught by any rep (genome)
+dt.never <- merged.dat
+dt.never[dt.never == 0] <- 8
+dt.never[dt.never <= 7] <- 0
+dt.never[dt.never == 8] <- 1
+
+# Always caught by all reps (genomes)
+dt.always <- merged.dat
+dt.always[dt.always < 7] <- 0
+dt.always[dt.always == 7] <- 1
+
+# Usually caught by most reps 3/7 or more (genomes)
+dt.usually <- merged.dat
+dt.usually[dt.usually <= 2] <- 0
+dt.usually[dt.usually > 2] <- 1
+
+
+# Save some upset plots of these 
+pdf(file="Upset_NeverCaught_10xDS.pdf", width=9, height=6)
+upset(dt.never, 
+      nsets =  5,
+      order.by="freq",
+      point.size = 2, 
+      line.size = 1, 
+      number.angles = 0,
+      sets.x.label = "N CpG sites", 
+      text.scale = c(1.2, 1.2, 1.2, 1.2, 1.1, 0.7))
+dev.off()
+
+pdf(file="Upset_AlwaysCaught_10xDS.pdf", width=9, height=6)
+upset(dt.always, 
+      nsets =  5,
+      order.by="freq",
+      point.size = 2, 
+      line.size = 1, 
+      number.angles = 0,
+      sets.x.label = "N CpG sites", 
+      text.scale = c(1.2, 1.2, 1.2, 1.2, 1.1, 0.7))
+dev.off()
+
+pdf(file="Upset_UsuallyCaught_10xDS.pdf", width=9, height=6)
+upset(dt.usually, 
+      nsets =  5,
+      order.by="freq",
+      point.size = 2, 
+      line.size = 1, 
+      number.angles = 0,
+      sets.x.label = "N CpG sites", 
+      text.scale = c(1.2, 1.2, 1.2, 1.2, 1.1, 0.7))
+dev.off()
+
+# Save annotations
+#1 always missed by TruSeq compared to caught by the others ! OBS THIS NOT DONE
+fwrite(anot[with(merged.dat, sums < 1),], file="TruSeq_specific_coords.bed", sep="\t", col.names=F)
+fwrite(anot[with(my.emseq, sums >= 4),], file="emseq_coords_catched.bed", sep="\t", col.names=F)
+
+
+
+# 2 always missed by Splat compared to the others:
+
+
